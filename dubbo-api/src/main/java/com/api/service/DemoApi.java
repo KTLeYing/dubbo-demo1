71c1805fbcac8240ec7a001a9a33357b28aec8a7
21989 <21989@tencent.com>
2022-05-10 02:28:05 +0800

    commit

---
+++ dubbo-api/src/main/java/com/api/service/DemoApi.java
@@ -0,0 +1,14 @@
+package com.api.service;
+
+/**
+ * @InterfaceName :   DemoApi
+ * @Description: 接口
+ * @Author: mzl
+ * @CreateDate: 2022/5/10 2:20
+ * @Version: 1.0
+ */
+public interface DemoApi {
+
+    public String sayHello();
+
+}
